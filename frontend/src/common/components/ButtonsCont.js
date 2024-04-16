c04d7450a2b217825c3f7573ded60e56df0b7af4
CLJiaHao <clarence.lee@murata.com>
2024-04-16 13:40:45 +0800

    fix naming for button

--- frontend/src/common/components/ButtonsCont.js
+++ frontend/src/common/components/ButtonsCont.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Button from "./Button";
 
-const ButtonsCont = ({ button_info, style }) => {
+const ButtonsCont = ({ button_info }) => {
   return (
     <div className="flex-center h-full w-full gap-5 px-3">
       {Object.keys(button_info).map((key) => (
         <div key={key} className="h-full w-full">
-          <Button style={style} button_info={button_info[key]} />
+          <Button name={key} button_info={button_info[key]} />
         </div>
       ))}
     </div>
