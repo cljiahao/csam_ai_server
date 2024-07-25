dd3ca9833337f77de83e2e7cf272bcae6bbd91c1
CLJiaHao <clarence.lee@murata.com>
2024-07-25 12:48:35 +0800

    Create a blank slate for version 2

--- backend/src/db/base.py
+++
@@ -1,3 +0,0 @@
-from db.base_class import Base
-from db.models.cai_ratio import CAI_RATIO
-from db.models.cdc_ratio import CDC_RATIO
--- backend/src/schemas/ratio.py
+++
@@ -1,9 +0,0 @@
-from schemas.chips import Details
-
-
-class CreateRatio(Details):
-    selected: dict
-
-
-class UpdateRatio(CreateRatio):
-    pass
