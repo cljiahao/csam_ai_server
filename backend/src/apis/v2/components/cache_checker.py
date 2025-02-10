73427fe02c579423276bf7090b1acba62e62b26f
CLJiaHao <clarence.lee@murata.com>
2025-02-10 13:25:44 +0800

    RFCT: refactor prints to logs and add error to imrpove maintainability

--- backend/src/apis/v2/components/cache_checker.py
+++ backend/src/apis/v2/components/cache_checker.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import Session
 from apis.v2.helpers.cache_utils import map_folder_files
 from apis.v2.schemas.base import CAIPage, CDCPage
 from apis.v2.schemas.files import DefectBatchDirectory
+from core.exceptions import CacheError
+from core.logging import logger
 from db.models.chip_details import ChipDetails
 from db.services.chip_details import ChipDetailsService
 from db.services.chip_lot_details import ChipLotDetailsService
@@ -26,20 +28,30 @@ def get_cache_if_exists(
         {"lot_no": lot_no, "plate_no": plate_no, "with_ai": page.ai}
     )
     if not chip_lot_details:
+        logger.info("Chip Lot Details not found, creating a new data.")
         return None
 
     result = map_folder_files(base_partial_path)
     if result is None:
-        return None
+        std_out = "Images stored previously not found."
+        logger.error(std_out)
+        raise CacheError(std_out)
+
     temp_dict, non_temp_dict = result
     folder_mapping = {**temp_dict, **non_temp_dict}
 
+    logger.info(f"uuid: {chip_lot_details.id}")
     chip_detail_service = ChipDetailsService(db)
     chip_details = chip_detail_service.read_chip_details(
         {"chip_lot_id": chip_lot_details.id}
     )
-    if not chip_details or len(folder_mapping) != len(chip_details):
+    if not chip_details:
+        logger.info("Chip Details not found or number of files don't match.")
         return None
+    if len(folder_mapping) != len(chip_details):
+        std_out = "Number of images stored in folder don't match number stored in DB."
+        logger.error(std_out)
+        raise CacheError(std_out)
 
     batch_data = group_chip_details_by_batch(chip_details)
 
