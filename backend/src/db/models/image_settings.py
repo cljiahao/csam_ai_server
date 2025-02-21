f080edc69d6129e11c151aa4418b586e965d1e65
CLJiaHao <clarence.lee@murata.com>
2025-02-21 14:21:00 +0800

    FEAT: add image settings database

--- backend/src/db/models/image_settings.py
+++ backend/src/db/models/image_settings.py
@@ -5,16 +5,16 @@ from sqlalchemy.orm import Mapped, mapped_column
 from db.base import Base
 
 
-class Settings(Base):
+class ImageSettings(Base):
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     date_created: Mapped[dt] = mapped_column(default=func.now())
     date_updated: Mapped[dt] = mapped_column(default=func.now(), onupdate=func.now())
     item: Mapped[str] = mapped_column(String, index=True)
-    batch_erode: Mapped[int]
-    batch_close: Mapped[int]
-    chip_erode: Mapped[int]
-    chip_close: Mapped[int]
-    chip_crop: Mapped[int]
+    batch_erode: Mapped[int] = mapped_column(default=0)
+    batch_close: Mapped[int] = mapped_column(default=0)
+    chip_erode: Mapped[int] = mapped_column(default=0)
+    chip_close: Mapped[int] = mapped_column(default=0)
+    crop_size: Mapped[int] = mapped_column(default=0)
 
     def __repr__(self):
         return f"<Settings(id={self.id}, item='{self.item}')>"
