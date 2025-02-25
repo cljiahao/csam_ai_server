b505042aa74f3a75a1578b49e882d21a6f80872b
CLJiaHao <clarence.lee@murata.com>
2025-02-25 17:07:02 +0800

    RFCT: downsize image_process functions for maintainability and readability

--- backend/src/apis/v2/components/image_process.py
+++ backend/src/apis/v2/components/image_process.py
@@ -1,24 +1,21 @@
-import cv2
 import numpy as np
 from sqlalchemy.orm import Session
 
 from apis.v2.helpers.image_process_utils import (
     check_single,
-    filter_contours,
+    create_border,
+    create_contour_list,
+    process_batch,
+    process_chip,
 )
-from apis.v2.helpers.processor.batch_processor import BatchProcessor
-from apis.v2.helpers.processor.chip_processor import ChipProcessor
 from apis.v2.helpers.processor.defect_processor import DefectProcessor
 from constants.chip_thresholds import ChipThreshold
-from constants.image_thresholds import ImageThreshold
 from core.exceptions import MissingSettings
 from db.models.image_settings import ImageSettings
 from db.services.image_settings import ImageSettingsService
 from schemas.chips_data import FileDataBatch
 from schemas.contours import ContourList
 from utils.debug import timer
-from utils.image_process.border_creator import BorderCreator
-from utils.image_process.contour_handler import ContourHandler
 from utils.image_process.mask_handler import MaskHandler
 from services.train import get_image_settings
 
@@ -29,6 +26,7 @@ def process_csam_image(
 ) -> tuple[dict[str, FileDataBatch], list, list]:
     """Main function for processing the input image."""
 
+    # Fetch image settings (either from API or fallback to DB)
     image_settings = get_or_fetch_image_settings(item, db)
 
     # Border Creation
@@ -58,16 +56,15 @@ def process_csam_image(
     # Defect Processing
     defect_processor = DefectProcessor(batch_processor, chip_processor, chip_threshold)
 
-    defect_batch_dict, to_predict_list, defect_list = process_chunk_contours(
+    base_file_name = f"{lot_no}_{plate_no}"
+
+    return (
         defect_processor,
-        lot_no,
-        plate_no,
+        base_file_name,
         refined_contours_info_list,
         border_image,
         border_pad,
     )
-
-    return defect_batch_dict, to_predict_list, defect_list
 
 
 @timer("Get Image Settings")
@@ -88,63 +85,21 @@ def get_or_fetch_image_settings(item: str, db: Session) -> ImageSettings:
     return image_settings
 
 
-@timer("Border creation")
-def create_border(image: np.ndarray, crop_size: int):
-    """Creates border images and returns relevant data."""
-    border_creator = BorderCreator(image, crop_size)
-    border_gray = border_creator.convert_background_white_and_grayscale()
-    border_blank = border_creator.create_blank_image()
-    border_pad = border_creator.border_pad
-
-    return border_creator.border_image, border_gray, border_blank, border_pad
-
-
-@timer("Batch processing")
-def process_batch(mask_handler: MaskHandler, image_settings: ImageSettings):
-    """Processes the image in batches."""
-    batch_processor = BatchProcessor(
-        mask_handler, image_settings.batch_erode, image_settings.batch_close
-    )
-    batch_processor.get_batch_data()
-    return batch_processor
-
-
-@timer("Chip processing")
-def process_chip(
-    mask_handler: MaskHandler, border_pad: int, image_settings: ImageSettings
-):
-    """Processes the chip data from the mask handler."""
-    chip_processor = ChipProcessor(
-        mask_handler,
-        image_settings.chip_erode,
-        image_settings.chip_close,
-        border_pad,
-        image_settings.crop_size,
-    )
-    return chip_processor
-
-
 @timer("Split and refining")
 def split_and_refine_contours(
     chip_threshold: ChipThreshold,
-    mask_image: np.ndarray,
+    chip_mask: np.ndarray,
     blank: np.ndarray,
     crop_size: int,
 ) -> ContourList:
     """Split and refine contours using BlobHandler."""
 
-    contours, _ = cv2.findContours(
-        mask_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
-    )
-
-    contour_info_list = ContourHandler.filter_and_build_contour_info(
-        contours, ImageThreshold.DENOISE_THRESHOLD
-    )
+    contour_info_list = create_contour_list(chip_mask)
 
     median_area = contour_info_list.get_median_area()
     chip_threshold.apply_ratios(median_area)
 
-    refined_contours = [
+    split_contours = [
         split_contour
         for contour_info in contour_info_list.contours
         for split_contour in check_single(
@@ -152,27 +107,12 @@ def split_and_refine_contours(
         ).contours
     ]
 
+    refined_contours = [
+        contour
+        for contour in split_contours
+        if chip_threshold.LOWER_CHIP_AREA
+        < contour.area
+        < chip_threshold.UPPER_CHIP_AREA
+    ]
+
     return ContourList(contours=refined_contours)
-
-
-@timer("Chunk contour processing")
-def process_chunk_contours(
-    defect_processor: DefectProcessor,
-    lot_no: str,
-    plate_no: str,
-    refined_contours_info_list: ContourList,
-    image: np.ndarray,
-    border_pad: int,
-):
-    """Processes contours in chunks, filtering and preparing defect data for further prediction."""
-    base_file_name = f"{lot_no}_{plate_no}"
-    chunked_contours = ContourHandler.chunking(refined_contours_info_list.contours)
-    defect_batch_dict, to_predict_list, defect_list = filter_contours(
-        defect_processor,
-        base_file_name,
-        image,
-        border_pad,
-        chunked_contours,
-    )
-
-    return defect_batch_dict, to_predict_list, defect_list
