48f3f41aba818eb50f574f6142dd54ad0661ee33
CLJiaHao <clarence.lee@murata.com>
2025-03-13 18:14:44 +0800

    RCFT: refactor function names from settings to image_settings to improve readability

--- backend/src/db/repository/image_settings.py
+++ backend/src/db/repository/image_settings.py
@@ -8,32 +8,32 @@ class ImageSettingsRepository(BaseRepository[ImageSettings]):
     def __init__(self, db: Session):
         super().__init__(db, ImageSettings)
 
-    def create_settings(self, settings_data: dict) -> ImageSettings:
-        """Create new chip settings."""
+    def create_image_settings(self, settings_data: dict) -> ImageSettings:
+        """Create new image settings."""
         return self.create(
             settings_data,
             print_message=f"Error creating settings from the database.",
         )
 
-    def read_settings(self, filter_condition: dict) -> ImageSettings:
-        """Read chip settings based on filter."""
+    def read_image_settings(self, filter_condition: dict) -> ImageSettings:
+        """Read image settings based on filter."""
         return self.read(
             filter_condition,
             print_message=f"Error reading settings from the database.",
         )
 
-    def update_settings(
+    def update_image_settings(
         self, filter_condition: dict, update_data: dict
     ) -> ImageSettings:
-        """Update chip settings with provided data."""
+        """Update image settings with provided data."""
         return self.update(
             filter_condition,
             update_data,
             print_message=f"Error updating settings in the database.",
         )
 
-    def delete_settings(self, filter_condition: dict) -> ImageSettings:
-        """Delete chip settings based on filter."""
+    def delete_image_settings(self, filter_condition: dict) -> ImageSettings:
+        """Delete image settings based on filter."""
         return self.delete(
             filter_condition,
             print_message=f"Error deleting settings from the database.",
