4a9fd23bf45453ca4f54685c1e66492bf3069113
CLJiaHao <clarence.lee@murata.com>
2025-03-17 16:46:09 +0800

    RFCT: move frontend services api calling for better maintain

--- frontend/src/services/api_files.js
+++ frontend/src/services/api_files.js
@@ -1,9 +1,15 @@
 import { createFileRequestOptions, createRequestOptions, sendRequest } from ".";
 
+export const getImageSrc = async (src_path) => {
+  const url = `/api/image/${src_path}`;
+  const options = createRequestOptions("GET");
+  return await sendRequest(url, options);
+};
+
 export const uploadImage = async (mode, item, lotNo, formData) => {
   const params = new URLSearchParams({
     item: item,
-    lot_no: lotNo
+    lot_no: lotNo,
   });
   const url = `/api/upload/process_image/${mode}?${params.toString()}`;
   const options = createFileRequestOptions("POST", formData);
