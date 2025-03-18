bcc22345ebf734b2962ada021d1dd86fb10d5166
CLJiaHao <clarence.lee@murata.com>
2025-03-18 13:33:24 +0800

    FEAT: create zustand global state for colors to be used

--- frontend/src/store/resetStore.js
+++ frontend/src/store/resetStore.js
@@ -1,7 +1,9 @@
+import { useColorStore } from "./color";
 import { useCoordStores, useImageStore } from "./display";
 import { useMarkerStore } from "./marker";
 
 export const resetStore = () => {
+  useColorStore.getState().resetColors();
   useCoordStores.getState().resetCoords();
   useImageStore.getState().resetImage();
   useMarkerStore.getState().resetMarks();
