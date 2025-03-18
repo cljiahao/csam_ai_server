d259705ee9d81c5481d02a147630c8ccf3b567ae
CLJiaHao <clarence.lee@murata.com>
2025-03-18 13:36:25 +0800

    FEAT: update usemarking to usecolors from colorpick

--- frontend/src/hooks/useMarking.js
+++ frontend/src/hooks/useMarking.js
@@ -1,9 +1,10 @@
-import { MARKERS } from "@/core/constants";
+import { useColorStore } from "@/store/color";
 import { useCoordStores } from "@/store/display";
 import { useMarkerStore } from "@/store/marker";
 import { useShallow } from "zustand/react/shallow";
 
 const useMarking = () => {
+  const colors = useColorStore((state) => state.colors);
   const { marks, addMark, updateMark, removeMark } = useMarkerStore(
     useShallow((state) => ({
       marks: state.marks,
@@ -12,24 +13,37 @@ const useMarking = () => {
       removeMark: state.removeMark,
     })),
   );
+
   const moveActive = useCoordStores((state) => state.moveActive);
 
   function onMark(e) {
     const file_name = e.currentTarget.id;
-    if (!moveActive) {
-      const stored_mark = marks.find((mark) => mark.file_name === file_name);
-      if (!stored_mark) {
-        addMark(file_name, MARKERS.colors[0]);
-        return;
-      }
-      const last_marker = stored_mark.marker === MARKERS.colors.at(-1);
-      if (last_marker) {
-        removeMark(file_name);
-        return;
-      }
-      updateMark(file_name, MARKERS.colors[stored_mark.marker.id]);
+    if (moveActive) return;
+    const stored_mark = marks.find((mark) => mark.file_name === file_name);
+    if (!stored_mark) {
+      const firstColor = colors[0];
+      addMark(file_name, {
+        name: firstColor.defect_label,
+        color: firstColor.hex_color,
+        radius: 1,
+      });
       return;
     }
+    const currentColor = stored_mark.marker.color;
+    const currentIndex = colors.findIndex(
+      (c) => c.hex_color.toLowerCase() === currentColor.toLowerCase(),
+    );
+    if (currentIndex === colors.length - 1) {
+      removeMark(file_name);
+      return;
+    }
+    const nextColor = colors[currentIndex + 1];
+    updateMark(file_name, {
+      name: nextColor.defect_label,
+      color: nextColor.hex_color,
+      radius: 1,
+    });
+    return;
   }
 
   return {
