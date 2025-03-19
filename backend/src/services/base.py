8d9963234f14b14b0c9db51345da096bdea43028
CLJiaHao <clarence.lee@murata.com>
2025-03-19 12:56:09 +0800

    FEAT: update API Client with file posting and update services using

--- backend/src/services/base.py
+++ backend/src/services/base.py
@@ -1,4 +1,6 @@
 import requests
+from pathlib import Path
+from contextlib import ExitStack
 
 from core.logging import logger
 
@@ -10,10 +12,11 @@ class APIClient:
         self.base_url = base_url
         self.timeout = timeout
         self.base_headers = {"Content-Type": "application/json"}
+        self.file_headers = {"Accept": "application/json"}
 
     def get(self, endpoint: str, headers: dict = None, params: dict = None):
         """Handles GET requests."""
-        url = f"{self.base_url}{endpoint}"
+        url = f"{self.base_url}/{endpoint.lstrip('/')}"
         combined_headers = {**self.base_headers, **(headers or {})}
         try:
             response = requests.get(
@@ -23,4 +26,50 @@ class APIClient:
             return response.json()
         except requests.RequestException as e:
             logger.error(f"GET request to {url} failed: {e}")
+            raise
+
+    def post(self, endpoint: str, data: dict = None, headers: dict = None):
+        """Handles POST requests."""
+        url = f"{self.base_url}/{endpoint.lstrip('/')}"
+        combined_headers = {**self.base_headers, **(headers or {})}
+        try:
+            response = requests.post(
+                url, json=data, headers=combined_headers, timeout=self.timeout
+            )
+            response.raise_for_status()
+            return response.json()
+        except requests.RequestException as e:
+            logger.error(f"POST request to {url} failed: {e}")
+            raise
+
+    def post_files(
+        self,
+        endpoint: str,
+        file_path_list: dict[str, Path],
+        data: dict = None,
+        headers: dict = None,
+    ):
+        """Handles POST requests with a file upload."""
+        url = f"{self.base_url}/{endpoint.lstrip('/')}"
+        combined_headers = {**self.file_headers, **(headers or {})}
+
+        try:
+            # Use ExitStack to manage multiple file context managers
+            with ExitStack() as stack:
+                files = {
+                    key: stack.enter_context(open(path, "rb"))
+                    for key, path in file_path_list.items()
+                }
+
+                response = requests.post(
+                    url,
+                    files=files,
+                    json=data,
+                    headers=combined_headers,
+                    timeout=self.timeout,
+                )
+                response.raise_for_status()
+                return response.json()
+        except requests.RequestException as e:
+            logger.error(f"POST file request to {url} failed: {e}")
             raise
