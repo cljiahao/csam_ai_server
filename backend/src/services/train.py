2303ea0cdd4218ff953457af1ecba77702f131d8
CLJiaHao <clarence.lee@murata.com>
2025-03-19 12:57:09 +0800

    FEAT: train services such as posting image files to train and checking health

--- backend/src/services/train.py
+++ backend/src/services/train.py
@@ -1,6 +1,10 @@
 import requests
+from pathlib import Path
 
+from apis.v2.schemas.files import FileDataBatchDirectory
+from constants.folder_names import FolderNames
 from core.config import service_settings
+from core.directory_manager import directory_manager as dm
 from services.base import APIClient
 
 
@@ -10,5 +14,47 @@ def get_image_settings(item: str) -> dict[str, str]:
     try:
         image_settings = api_client.get(f"/api/v2/settings/image?item={item}")
         return image_settings
+    except requests.RequestException as e:
+        return None
+
+
+def post_image_file(
+    item: str,
+    lot_no: str,
+    defect_batch_directory: FileDataBatchDirectory,
+):
+
+    api_client = APIClient(service_settings.AI_TRAIN_URL)
+    try:
+        image_file_name = Path(defect_batch_directory.directory).stem
+        original_path = (
+            dm.images_dir
+            / defect_batch_directory.directory
+            / FolderNames.ORIGINAL.value
+        )
+        file_path_list = {
+            "file": original_path / f"{image_file_name}.png",
+        }
+
+        data = {
+            "item": item,
+            "lot_no": lot_no,
+            "defect_batch_directory": defect_batch_directory,
+        }
+
+        image_settings = api_client.post_files(
+            "/api/v2/image/process_image", file_path_list=file_path_list, data=data
+        )
+        return image_settings
+    except requests.RequestException as e:
+        raise Exception(f"Failed to post model files: {e}")
+
+
+def train_health_check():
+
+    api_client = APIClient(service_settings.AI_TRAIN_URL)
+    try:
+        health_res = api_client.get("/api/v2/health")
+        return bool((health_res or {}).get("status") == "OK")
     except requests.RequestException as e:
         return None
