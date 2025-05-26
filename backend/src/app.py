3d7db7663205e4d6166c146aa786ff8b02871730
CLJiaHao <clarence.lee@murata.com>
2025-05-26 13:28:21 +0800

    FEAT: add error handling for http and moved db tables creation to under db

--- backend/src/app.py
+++ backend/src/app.py
@@ -4,17 +4,16 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from apis.routes import router
 from core.config import common_settings, api_settings
-from db.base import Base
-from db.session import engine
-
-
-def create_tables() -> None:
-    """Create database tables based on the metadata."""
-    Base.metadata.create_all(bind=engine)
+from db.base import create_tables
+from error_handler import configure_exceptions
 
 
 def configure_cors(app: FastAPI) -> None:
-    """Configure CORS settings for the FastAPI application."""
+    """Configures Cross-Origin Resource Sharing (CORS) middleware for the FastAPI application.
+
+    Args:
+        app: The FastAPI application instance.
+    """
     origins = api_settings.ALLOWED_CORS
 
     app.add_middleware(
@@ -26,21 +25,17 @@ def configure_cors(app: FastAPI) -> None:
     )
 
 
-def include_router(app: FastAPI) -> None:
-    """Include application routers."""
-    app.include_router(router)
-
-
-# TODO: add metadatas (Tags,Summary,Description) to fastapi
-
-
 def start_application() -> FastAPI:
-    """Initialize and configure the FastAPI application."""
+    """Initialize and configures the FastAPI application.
+
+    Returns:
+        The initialized and configured FastAPI application instance.
+    """
     app = FastAPI(
         title=common_settings.PROJECT_NAME,
         version=common_settings.PROJECT_VERSION,
         description=textwrap.dedent(common_settings.PROJECT_DESCRIPTION),
-        root_path="/api",
+        root_path=f"/{api_settings.FASTAPI_ROOT}",
         swagger_ui_parameters={
             "defaultModelsExpandDepth": -1,  # Hide models section by default
             "docExpansion": "none",  # Collapse all sections by default
@@ -48,7 +43,8 @@ def start_application() -> FastAPI:
     )
 
     configure_cors(app)
-    include_router(app)
+    configure_exceptions(app)
+    app.include_router(router)
     create_tables()
 
     return app
@@ -56,14 +52,3 @@ def start_application() -> FastAPI:
 
 # Initialize the FastAPI application
 app = start_application()
-
-
-@app.get(
-    "/",
-    tags=["home"],
-    summary="Home Route",
-    description="A simple home route returning a welcome message.",
-)
-def home() -> dict[str, str]:
-    """Simple home route."""
-    return {"msg": "Hello Fast_API 🚀"}
