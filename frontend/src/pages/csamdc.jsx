67c06c02f5784c03f2fdb8d7d181bd6023a8d0df
CLJiaHao <clarence.lee@murata.com>
2025-06-06 16:18:32 +0800

    FEAT: update pages to use the latest features

--- frontend/src/pages/csamdc.jsx
+++ frontend/src/pages/csamdc.jsx
@@ -1,12 +1,14 @@
 import BaseLayout from "@/components/layouts/BaseLayout";
 import { Separator } from "@/components/ui/separator";
-import { Gallery, ImageHolder, InfoBar } from "@/features";
+import Gallery from "@/features/gallery/Gallery";
+import ImageDisplay from "@/features/image-display/ImageDisplay";
+import InfoBar from "@/features/info-bar/InfoBar";
 
 const CsamDC = () => {
   return (
     <BaseLayout>
       <section className="w-full">
-        <ImageHolder />
+        <ImageDisplay />
       </section>
       <aside className="flex h-full w-3/5 flex-col border-l-2 border-slate-400">
         <InfoBar />
--- frontend/src/pages/index.js
+++
@@ -1,5 +0,0 @@
-import CsamAI from "./csamai";
-import CsamDC from "./csamdc";
-import Home from "./home";
-
-export { CsamAI, CsamDC, Home };
