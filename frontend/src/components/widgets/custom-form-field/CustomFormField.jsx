f07fb8891dacce8bdfe568f452c02821f7d78a2f
CLJiaHao <clarence.lee@murata.com>
2025-06-06 16:17:03 +0800

    FEAT: prevent cloneElement from replacing props stated in parent

--- frontend/src/components/widgets/custom-form-field/CustomFormField.jsx
+++ frontend/src/components/widgets/custom-form-field/CustomFormField.jsx
@@ -27,31 +27,63 @@ const CustomFormField = ({
     <FormField
       name={name}
       control={control}
-      render={({ field }) => (
-        <FormItem className="flex-center hw-full flex-col p-2">
-          <div className={cn("flex-center w-full", className)}>
-            <FormLabel className="w-1/4">{label}</FormLabel>
-            {popoverTrigger ? (
-              <Popover>
-                <PopoverTrigger>
-                  <FormControl>
-                    {cloneElement(popoverTrigger, { ...field })}
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent>
-                  {cloneElement(children, { ...field })}
-                </PopoverContent>
-              </Popover>
-            ) : (
-              <FormControl className="w-3/4">
-                {cloneElement(children, { ...field })}
-              </FormControl>
-            )}
-          </div>
-          <FormDescription />
-          <FormMessage className="flex-center" />
-        </FormItem>
-      )}
+      render={({ field }) => {
+        const createMergedProps = (originalElement, fieldProps) => {
+          const merged = { ...fieldProps };
+          if (!originalElement) {
+            return merged;
+          }
+          const originalProps = originalElement.props;
+
+          for (const key in originalProps) {
+            if (Object.prototype.hasOwnProperty.call(originalProps, key)) {
+              const originalValue = originalProps[key];
+              const fieldValue = fieldProps[key];
+              if (
+                typeof originalValue === "function" &&
+                typeof fieldValue === "function"
+              )
+                merged[key] = (...args) => {
+                  originalValue(...args);
+                  fieldValue(...args);
+                };
+            }
+          }
+          return merged;
+        };
+
+        const childrenMergedProps = createMergedProps(children, field);
+        let popoverTriggerMergedProps = {};
+        if (popoverTrigger) {
+          popoverTriggerMergedProps = createMergedProps(popoverTrigger, field);
+        }
+
+        return (
+          <FormItem className="flex-center hw-full flex-col space-y-0">
+            <div className={cn("flex-center w-full", className)}>
+              {label && <FormLabel className="w-1/4">{label}</FormLabel>}
+              {popoverTrigger ? (
+                <Popover>
+                  <PopoverTrigger>
+                    <FormControl>
+                      {cloneElement(popoverTrigger, popoverTriggerMergedProps)}
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent>
+                    {cloneElement(children, childrenMergedProps)}
+                  </PopoverContent>
+                </Popover>
+              ) : (
+                <FormControl className="w-3/4">
+                  {cloneElement(children, childrenMergedProps)}
+                </FormControl>
+              )}
+            </div>
+            <FormDescription />
+            <FormMessage className="flex-center" />
+          </FormItem>
+        );
+      }}
     />
   );
 };
