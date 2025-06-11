ed1be660a02c3e6dab0eb391df93fbfe13ee9584
CLJiaHao <clarence.lee@murata.com>
2025-06-11 15:51:25 +0800

    BUG: fix proper usage of APIClient for calling to prass

--- backend/src/services/prass.py
+++ backend/src/services/prass.py
@@ -1,8 +1,12 @@
+import urllib.parse
+
 from core.config import service_settings
 from core.exceptions import NoResultsFound
 from core.logging import logger
 from services.base import APIClient
 from utils.debug import error_handler
+
+API_PRASS_ITEM_ENDPOINT = "/data"
 
 
 @error_handler()
@@ -17,7 +21,8 @@ def check_lot(lot_no: str) -> str | None:
         return None
 
     api_client = APIClient(service_settings.PRASS_URL)
-    prass_data = api_client.get(lot_no)
+    search_params = urllib.parse.urlencode({"lotNo": lot_no})
+    prass_data = api_client.get(f"{API_PRASS_ITEM_ENDPOINT}?{search_params}")
 
     if not isinstance(prass_data, dict):
         raise NoResultsFound(f"Lot number: {lot_no} not found in PRASS Server.")
